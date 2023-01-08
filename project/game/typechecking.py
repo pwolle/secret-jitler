24596bc554b40785ae703b3c3bd0333d37702673
Kjell Jacob <kjelljacob@MacBook-Air-von-Kjell.local>
2023-01-08 16:18:24 +0100

    KJ typechecking jitified

--- project/game/typechecking.py
+++ project/game/typechecking.py
@@ -1,13 +1,14 @@
+import jax
 import shtypes
 import jax.numpy as jnp
-from typing import Union
 from jaxtyping import jaxtyped
 from typeguard import typechecked
 
 
+@jax.jit
 @jaxtyped
 @typechecked
-def check_player_num(player_num: shtypes.player_num) -> bool:
+def check_player_num(player_num: shtypes.player_num) -> shtypes.jbool:
     """
     Check the type player_num (from shtypes).
 
@@ -16,20 +17,21 @@ def check_player_num(player_num: shtypes.player_num) -> bool:
             Number of players. Should be between 5 and 10.
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff player_num is in a valid state.
     """
-    works: bool = player_num in jnp.arange(5, 11)
+    works = jnp.logical_and(player_num >= 5, player_num <= 10)
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
 def check_player(
         player_num: shtypes.player_num,
         player: shtypes.player
-) -> bool:
+) -> shtypes.jbool:
     """
     Check the type player (from shtypes).
 
@@ -40,17 +42,21 @@ def check_player(
             Number of player. Should be between 0 and the number of players.
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff player is in a valid state.
     """
-    works: bool = player in jnp.arange(0, player_num)
+    works = jnp.logical_and(player >= 0, player < player_num)
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
-def check_roles(player_num: shtypes.player_num, roles: shtypes.roles) -> bool:
+def check_roles(
+        player_num: shtypes.player_num,
+        roles: shtypes.roles
+) -> shtypes.jbool:
     """
     Check the type roles (from shtypes).
 
@@ -69,24 +75,28 @@ def check_roles(player_num: shtypes.player_num, roles: shtypes.roles) -> bool:
             Fascists | 1+H | 1+H | 2+H | 2+H | 3+H | 3+H |
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff roles is in a valid state.
     """
+    # roles must have length player_num
+    right_length = roles.size == player_num
+
     # there can only be one H
-    right_num_h: bool = jnp.equal(jnp.count_nonzero(roles == 2), 1)
+    right_num_h = jnp.count_nonzero(roles == 2) == 1
 
     # according to the table we get this correlation
-    right_sum: bool = jnp.equal(jnp.sum(roles), jnp.ceil(player_num / 2))
+    right_sum = jnp.sum(roles) == jnp.ceil(player_num / 2)
 
     # all checks need to pass for roles to be in a valid state
-    works = bool(right_num_h * right_sum)
+    works = right_length * right_num_h * right_sum
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
-def check_board(board: shtypes.board) -> bool:
+def check_board(board: shtypes.board) -> shtypes.jbool:
     """
     Check the type board (from shtypes).
 
@@ -96,21 +106,24 @@ def check_board(board: shtypes.board) -> bool:
             board[0] should be in [0, 1, ..., 5], board[1] in [0, 1, ..., 6].
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff board works is in a valid state.
     """
-    works = bool(
-        jnp.all(jnp.array(
-            [board[0] in jnp.arange(0, 6), board[1] in jnp.arange(0, 7)]
-        ))
-    )
+    right_interval_l = jnp.logical_and(board[0] >= 0, board[0] <= 5)
+
+    right_interval_h = jnp.logical_and(board[1] >= 0, board[1] <= 6)
+
+    works = right_interval_l * right_interval_h
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
-def check_pile(pile: Union[shtypes.pile_draw, shtypes.pile_discard]) -> bool:
+def check_pile(
+        pile: shtypes.pile_draw | shtypes.pile_discard
+) -> shtypes.jbool:
     """
     Check the type pile_draw or pile_discard (from shtypes).
 
@@ -121,25 +134,26 @@ def check_pile(pile: Union[shtypes.pile_draw, shtypes.pile_discard]) -> bool:
             second.
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff the pile is in a valid state.
     """
-    right_num_l: bool = pile[0] in jnp.arange(0, 7)
+    right_num_l = jnp.logical_and(pile[0] >= 0, pile[0] <= 6)
 
-    right_num_f: bool = pile[1] in jnp.arange(0, 12)
+    right_num_f = jnp.logical_and(pile[1] >= 0, pile[1] <= 11)
 
-    works = bool(right_num_l * right_num_f)
+    works = right_num_l * right_num_f
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
 def check_piles_board(
         pile_draw: shtypes.pile_draw,
         pile_discard: shtypes.pile_discard,
         board: shtypes.board
-) -> bool:
+) -> shtypes.jbool:
     """
     Check if the state of both piles is matching the state of the board.
     There should always be the same amount of cards.
@@ -153,73 +167,75 @@ def check_piles_board(
             The board state (how many policies each party enacted).
             
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff the states of both piles and the board are valid.
     """
-    right_sum_l: bool = pile_draw[0] + pile_discard[0] + board[0] == 6
+    right_sum_l = pile_draw[0] + pile_discard[0] + board[0] == 6
 
-    right_sum_h: bool = pile_draw[1] + pile_discard[1] + board[1] == 11
+    right_sum_h = pile_draw[1] + pile_discard[1] + board[1] == 11
 
-    works = bool(right_sum_l * right_sum_h)
+    works = right_sum_l * right_sum_h
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
-def check_president(
+def check_president_or_chancelor(
         player_num: shtypes.player_num,
+        role: shtypes.president | shtypes.chancelor
+) -> shtypes.jbool:
+    """
+    Check the type president or chancelor (from shtypes).
+
+    Args:
+        player_num: shtypes.player_num
+            Number of players.
+        role: shtypes.president or shtypes.chancelor (jint)
+            Player number of the president or chancelor. Should be between 0 and
+            player_num - 1.
+
+    Returns:
+        works: shtypes.jbool
+            True iff the role is in a valid state.
+    """
+    works = jnp.logical_and(role >= 0, role < player_num)
+
+    return works
+
+
+@jax.jit
+@jaxtyped
+@typechecked
+def check_chancelor_and_president(
+        chancelor: shtypes.chancelor,
         president: shtypes.president
-) -> bool:
+) -> shtypes.jbool:
     """
-    Check the type president (from shtypes).
+    Check if the types chancelor and president (from shtypes)
+    are in a valid state.
 
     Args:
-        player_num: shtypes.player_num
-            Number of players.
-        president: shtypes.president (jint)
-            Player number of the president. Should be between 0 and
-            player_num - 1.
-
-    Returns:
-        works: bool
-            True iff president is in a valid state.
-    """
-    works: bool = president in jnp.arange(0, player_num)
-
-    return works
-
-
-@jaxtyped
-@typechecked
-def check_chancelor(
-        player_num: shtypes.player_num,
-        chancelor: shtypes.chancelor
-) -> bool:
-    """
-    Check the type chancelor (from shtypes).
-
-    Args:
-        player_num: shtypes.player_num
-            Number of players.
         chancelor: shtypes.chancelor (jint)
-            Player number of the chancelor. Should be between 0 and
-            player_num - 1.
+            Player number of the chancelor.
+        president
 
     Returns:
-        works: bool
-            True iff chancelor is in a valid state.
+        works: shtypes.jbool
+            True iff chancelor and president are in a valid state.
     """
-    works: bool = chancelor in jnp.arange(0, player_num)
+    works = jnp.logical_not(chancelor == president)
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
 def check_election_tracker(
         election_tracker: shtypes.election_tracker
-) -> bool:
+) -> shtypes.jbool:
     """
     Check the type election_tracker (from shtypes).
 
@@ -228,29 +244,62 @@ def check_election_tracker(
             State of the election. Should be in [0, 1, 2].
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff election_tracker is in a valid state.
     """
-    works: bool = election_tracker in jnp.arange(0, 3)
+    works = jnp.logical_and(election_tracker >= 0, election_tracker <= 2)
 
     return works
 
 
+@jax.jit
 @jaxtyped
 @typechecked
-def check_killed(killed: shtypes.killed) -> bool:
+def check_killed(
+        player_num: shtypes.player_num,
+        killed: shtypes.killed
+) -> shtypes.jbool:
     """
     Check the type killed (from shtypes).
 
     Args:
+        player_num: shtypes.player_num
+            Number of players.
         killed: shtypes.killed (jtp.Bool[jtp.Array, "player_num"])
             State of livelihood of each player. killed[i] should be True iff
             player i is killed.
 
     Returns:
-        works: bool
+        works: shtypes.jbool
             True iff killed is in a valid state.
     """
-    works = not jnp.all(killed)
+    right_length = killed.size == player_num
+
+    right_amount_dead = jnp.logical_and(killed.sum() >= 0, killed.sum() <= 2)
+
+    works = right_length * right_amount_dead
+
+    return works
+
+
+@jax.jit
+@jaxtyped
+@typechecked
+def check_winner(winner: shtypes.winner) -> shtypes.jbool:
+    """
+    Check the type winner (from shtypes).
+
+    Args:
+        winner: shtypes.winner (jtp.Bool[jtp.Array, "2"])
+            The boolean array showing which party won. First index is True iff
+            liberals won, second iff fascists won. Can both be False but never
+            [True, True].
+
+    Returns:
+        works: shtypes.jbool
+            True iff winner is in a valid state.
+
+    """
+    works = jnp.logical_and(winner.sum() >= 0, winner.sum() <= 1)
 
     return works
