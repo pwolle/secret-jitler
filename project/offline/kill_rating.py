4556f866c4a81346bf16cdcc3a7486800e8029a4
unitdeterminant <ihaveunitdeterminant@gmail.com>
2023-01-20 22:04:50 +0100

    restructured folders

--- project/offline/kill_rating.py
+++ project/offline/kill_rating.py
@@ -3,12 +3,12 @@ import jax.numpy as jnp
 import jax.random as jrn
 import jaxtyping as jtp
 
-from . import run
+from ..game import run
+
 
 @jax.jit
 @jaxtyping.typeguard
-def kill_rating_single(player_num:int, time, history) -> jtp.Float[jnp.ndarray, 'players']:
-
+def kill_rating_single(player_num: int, time, history) -> jtp.Float[jnp.ndarray, 'players']:
     """
     rates/weights the kill-probability-choice for one move of one player
 
@@ -25,28 +25,27 @@ def kill_rating_single(player_num:int, time, history) -> jtp.Float[jnp.ndarray, 
     """
 
     # get dead people of the current and last round
-    killed=history['killed'][time]
-    prekilled=history['killed'][time-1]
+    killed = history['killed'][time]
+    prekilled = history['killed'][time-1]
 
     # get the role of the current player
-    player_role = history['roles'].at[0,player_num].get()
+    player_role = history['roles'].at[0, player_num].get()
 
     # check which party won the game eventually
-    l_won = history['winner'][0,0]
-    f_won = history['winner'][0,1]
+    l_won = history['winner'][0, 0]
+    f_won = history['winner'][0, 1]
     no_winner = jnp.logical_not(jnp.logical_xor(f_won, l_won))
 
     # check if the player is on the winning side
-    player_win = jnp.logical_or(jnp.logical_and(l_won, player_role==0),
-                                jnp.logical_and(f_won , player_role >0))
+    player_win = jnp.logical_or(jnp.logical_and(l_won, player_role == 0),
+                                jnp.logical_and(f_won, player_role > 0))
     player_lose = jnp.logical_not(player_win)*jnp.logical_not(no_winner)
 
     # get number of players
     players = jnp.shape(history['roles'])[1]
 
-
     # was a player killed, if yes: who?
-    difference = killed==prekilled
+    difference = killed == prekilled
     kill = jnp.logical_not(jnp.all(difference))
     shot = jnp.sum(jnp.arange(players)*jnp.logical_not(difference))
 
@@ -59,71 +58,64 @@ def kill_rating_single(player_num:int, time, history) -> jtp.Float[jnp.ndarray, 
     We dont change anything in the normal distribution if we dont know the game result yet.
     Afterwards we normalize the probs and return
     """
-    base_array=jnp.array([1]*players) *jnp.logical_not(kill)
-    good_idea = jnp.zeros(players).at[shot].set(1) *kill
+    base_array = jnp.array([1]*players) * jnp.logical_not(kill)
+    good_idea = jnp.zeros(players).at[shot].set(1) * kill
     bad_idea = jnp.ones(players).at[shot].set(0) * kill
-    prob_out = good_idea *player_win + bad_idea * player_lose
+    prob_out = good_idea * player_win + bad_idea * player_lose
     prob_out += base_array
     return prob_out*(1/jnp.sum(prob_out))
 
 
-
 @jax.jit
 @jaxtyping.typeguard
-def kill_rating_game(player_num:int,history) -> jtp.Float[jnp.ndarray,'history players']:
-
+def kill_rating_game(player_num: int, history) -> jtp.Float[jnp.ndarray, 'history players']:
     """
     'iterate' over the game for one fixed player
     """
 
     # please dont question `+1` and `[1:]`:
     # the structure of `history['killed']` needs some special attention and I'm pretty confident this works
-    game_leng=jnp.shape(history['killed'])[0]+1
+    game_leng = jnp.shape(history['killed'])[0]+1
     return jax.vmap(
-        kill_rating_single,in_axes=(None,0, None))(player_num, jnp.arange(game_leng),history)[1:]
+        kill_rating_single, in_axes=(None, 0, None))(player_num, jnp.arange(game_leng), history)[1:]
 
 
 @jax.jit
 @jaxtyping.typeguard
-def kill_rating_all(state) -> jtp.Float[jnp.ndarray,'players history players']:
-
+def kill_rating_all(state) -> jtp.Float[jnp.ndarray, 'players history players']:
     """
     'iterate' over the game for all players
     """
     history = {}
-    for k,v in state.items():
-        history[k]=v[-1]
+    for k, v in state.items():
+        history[k] = v[-1]
 
     player_num = jnp.shape(history['roles'])[1]
-    return jax.vmap(kill_rating_game,in_axes=(0,None))(jnp.arange(player_num),history)
-
+    return jax.vmap(kill_rating_game, in_axes=(0, None))(jnp.arange(player_num), history)
 
 
 @jax.jit
 @jaxtyping.typeguard
 def prop_rating_single(player_num: int, prop: int, history) -> jtp.Float[jnp.ndarray, 'players']:
-
     """
     rates the proposal-rate for a single player in one move
     """
 
-    player_role = history['roles'].at[0,player_num].get()
-    l_won = history['winner'][0,0]
-    f_won = history['winner'][0,1]
+    player_role = history['roles'].at[0, player_num].get()
+    l_won = history['winner'][0, 0]
+    f_won = history['winner'][0, 1]
     no_winner = jnp.logical_not(jnp.logical_xor(f_won, l_won))
 
-    player_win = jnp.logical_or(jnp.logical_and(l_won, player_role==0),
-                                jnp.logical_and(f_won , player_role >0))
+    player_win = jnp.logical_or(jnp.logical_and(l_won, player_role == 0),
+                                jnp.logical_and(f_won, player_role > 0))
     player_lose = jnp.logical_not(player_win)*jnp.logical_not(no_winner)
     players = jnp.shape(history['roles'])[1]
 
-
-    base_array=jnp.array([1]*players)
+    base_array = jnp.array([1]*players)
     good_idea = jnp.zeros(players).at[prop].set(1)
     bad_idea = jnp.ones(players).at[prop].set(0)
 
-
-    prob_out = good_idea *player_win + bad_idea * player_lose
+    prob_out = good_idea * player_win + bad_idea * player_lose
     prob_out += base_array * no_winner
 
     return prob_out*(1/jnp.sum(prob_out))
@@ -131,17 +123,17 @@ def prop_rating_single(player_num: int, prop: int, history) -> jtp.Float[jnp.nda
 
 @jax.jit
 @jaxtyping.typeguard
-def prop_rating_game(player_num,history)-> jtp.Float[jnp.ndarray,'history players']:
+def prop_rating_game(player_num, history) -> jtp.Float[jnp.ndarray, 'history players']:
 
-    return jax.vmap(prop_rating_single,in_axes=(None,0, None))(player_num, history['proposed'],history)
+    return jax.vmap(prop_rating_single, in_axes=(None, 0, None))(player_num, history['proposed'], history)
+
 
 @jax.jit
 @jaxtyping.typeguard
-def prop_rating_all(state) -> jtp.Float[jnp.ndarray,'players history players']:
+def prop_rating_all(state) -> jtp.Float[jnp.ndarray, 'players history players']:
     history = {}
-    for k,v in state.items():
-        history[k]=v[-1]
+    for k, v in state.items():
+        history[k] = v[-1]
 
     player_num = jnp.shape(history['roles'])[1]
-    return jax.vmap(prop_rating_game,in_axes=(0,None))(jnp.arange(player_num),history)
-
+    return jax.vmap(prop_rating_game, in_axes=(0, None))(jnp.arange(player_num), history)
