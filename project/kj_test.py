a151afdb0d183129c39d1b03095c576ceef9c2ad
Kjell Jacob <kjelljacob@MacBook-Air-von-Kjell.local>
2023-01-26 21:17:33 +0100

    interactive.py bugs fixed

--- project/kj_test.py
+++ project/kj_test.py
@@ -1,8 +1,7 @@
 import jax.random as jrn
-from game import narrate
 from bots import bots, run, interactive
 
-key = jrn.PRNGKey(1283722)
+key = jrn.PRNGKey(1213722)
 key, subkey = jrn.split(key)
 
 propose_bot = run.fuse(
@@ -12,9 +11,9 @@ propose_bot = run.fuse(
 )
 
 vote_bot = run.fuse(
-    bots.vote_no,
-    bots.vote_no,
-    bots.vote_no
+    bots.vote_yes,
+    bots.vote_yes,
+    bots.vote_yes
 )
 
 presi_bot = run.fuse(
@@ -35,18 +34,8 @@ shoot_bot = run.fuse(
     bots.shoot_random
 )
 
-run_func = run.closure(
-    10,
-    30,
-    propose_bot=propose_bot,
-    vote_bot=vote_bot,
-    presi_bot=presi_bot,
-    chanc_bot=chanc_bot,
-    shoot_bot=shoot_bot
-)
-
 run_func_interactive = interactive.closure(
-    10,
+    7,
     30,
     propose_bot=propose_bot,
     vote_bot=vote_bot,
@@ -56,15 +45,11 @@ run_func_interactive = interactive.closure(
 )
 
 params = {
-    'propose': 10**3 - 1,
+    'propose': 10**2 - 1,
     'vote': 10**3 - 1,
-    'presi': 10**3 - 1,
+    'presi': 10**2 - 1,
     'chanc': 10**3 - 1,
-    'shoot': 10**3 - 1
+    'shoot': 10**2 - 1
 }
 
-#state = run_func(subkey, params)
-
-state = run_func_interactive(subkey, 7, params)
-
-#narrate.narrate_game(state)
+state = run_func_interactive(subkey, 3, params, 1.0)
